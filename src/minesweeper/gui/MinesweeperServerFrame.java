6ede4337dad846e4fdac9bbadaf2cc827a1a3259
Manuel Guillen <manuelg@mit.edu>
2016-05-16 05:23:25 -0400

    Updated graphic interface.
    Made "Random" and "File" options mutually exclusive options.
    Made "Browse..." button and label with selected file name for "File"
    board option.
    Made sliders with labels for columns and rows for "Random" board option.
    Added listeners to radio buttons and to sliders to respond to changing
    radio button options (allowing switching which of the two above options
    to display, and updating slider count labels)

--- src/minesweeper/gui/MinesweeperServerFrame.java
+++ src/minesweeper/gui/MinesweeperServerFrame.java
@@ -1,32 +1,49 @@
 package minesweeper.gui;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  * Represents a graphic user interface for managing a
  * Minesweeper server.
  */
 @SuppressWarnings("serial")
-public class MinesweeperServerFrame extends JFrame {
-
-    private JPanel contentPane;
-    private JTextField addressField;
-    private JTextField portField;
-    private JCheckBox kickCheckBox;
-    private JRadioButton randomBoardButton;
-    private JRadioButton fileBoardButton;
+public class MinesweeperServerFrame extends JFrame implements ActionListener, ChangeListener {
+    
+    private static final Font HEADER_FONT = new Font("Tahoma", Font.BOLD, 18);
+    private static final Font LABEL_FONT = new Font("Tahoma", Font.PLAIN, 14);
+    private static final Font RESULT_FONT = new Font("Tahoma", Font.PLAIN, 13);
+    
+    private static final int DEFAULT_BOARD_SIZE = 10;
+    private static final int MAX_BOARD_SIZE = 30;
+    
+    private final JPanel contentPane;
+    
+    private final JTextField portField;
+    private final JCheckBox kickCheckBox;
+    private final JRadioButton randomBoardButton, fileBoardButton;
+    private final JButton browseButton;
+    private final JLabel fileLabel;
+    private final JLabel rowsLabel, columnsLabel, rowNumLabel, columnNumLabel;
+    private final JSlider rowSlider, columnSlider;
 
     /**
      * Create the Minesweeper Server GUI.
@@ -34,7 +51,7 @@ public class MinesweeperServerFrame extends JFrame {
      */
     public MinesweeperServerFrame() throws UnknownHostException {
         setTitle("Minesweeper Server");
-        setBounds(100, 100, 460, 300);
+        setBounds(100, 100, 475, 300);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
@@ -45,25 +62,25 @@ public class MinesweeperServerFrame extends JFrame {
         
         JLabel headerLabel = new JLabel("Minesweeper Server Information");
         headerLabel.setBounds(12, 13, 305, 22);
-        headerLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
+        headerLabel.setFont(HEADER_FONT);
         contentPane.add(headerLabel);
         
         JLabel addressLabel = new JLabel("IP Address:");
-        addressLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        addressLabel.setFont(LABEL_FONT);
         addressLabel.setBounds(12, 50, 74, 16);
         contentPane.add(addressLabel);
         
         JLabel portLabel = new JLabel("Port:");
-        portLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        portLabel.setFont(LABEL_FONT);
         portLabel.setBounds(221, 50, 36, 16);
         contentPane.add(portLabel);
         
         JLabel boardLabel = new JLabel("Board:");
-        boardLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        boardLabel.setBounds(12, 79, 47, 16);
+        boardLabel.setFont(LABEL_FONT);
+        boardLabel.setBounds(12, 81, 47, 16);
         contentPane.add(boardLabel);
         
-        addressField = new JTextField();
+        JTextField addressField = new JTextField();
         addressField.setEditable(false);
         addressField.setText(Inet4Address.getLocalHost().getHostAddress().toString());
         addressField.setBounds(86, 48, 123, 22);
@@ -77,28 +94,109 @@ public class MinesweeperServerFrame extends JFrame {
         kickCheckBox = new JCheckBox("Kick Upon Loss:");
         kickCheckBox.setFocusable(false);
         kickCheckBox.setSelected(true);
-        kickCheckBox.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        kickCheckBox.setFont(LABEL_FONT);
         kickCheckBox.setHorizontalTextPosition(SwingConstants.LEADING);
-        kickCheckBox.setBounds(322, 46, 131, 25);
+        kickCheckBox.setBounds(338, 46, 126, 25);
         contentPane.add(kickCheckBox);
         
         ButtonGroup buttonGroup = new ButtonGroup();
         
         randomBoardButton = new JRadioButton("Random");
         randomBoardButton.setSelected(true);
-        randomBoardButton.setBounds(67, 76, 81, 25);
+        randomBoardButton.setBounds(67, 78, 81, 25);
+        randomBoardButton.addActionListener(this);
         buttonGroup.add(randomBoardButton);
         contentPane.add(randomBoardButton);
         
         fileBoardButton = new JRadioButton("File");
-        fileBoardButton.setBounds(155, 76, 54, 25);
+        fileBoardButton.setBounds(155, 78, 54, 25);
+        fileBoardButton.addActionListener(this);
         buttonGroup.add(fileBoardButton);
         contentPane.add(fileBoardButton);
         
+        rowsLabel = new JLabel("Rows:");
+        rowsLabel.setFont(LABEL_FONT);
+        rowsLabel.setBounds(221, 81, 47, 16);
+        contentPane.add(rowsLabel);
+        
+        columnsLabel = new JLabel("Columns:");
+        columnsLabel.setFont(LABEL_FONT);
+        columnsLabel.setBounds(221, 107, 64, 16);
+        contentPane.add(columnsLabel);
+        
+        rowSlider = new JSlider(1,MAX_BOARD_SIZE,DEFAULT_BOARD_SIZE);
+        rowSlider.setBounds(284, 78, 158, 26);
+        rowSlider.setFocusable(false);
+        rowSlider.setMajorTickSpacing(10);
+        rowSlider.setMinorTickSpacing(2);
+        rowSlider.setPaintTicks(true);
+        rowSlider.addChangeListener(this);
+        contentPane.add(rowSlider);
+        
+        columnSlider = new JSlider(1,MAX_BOARD_SIZE,DEFAULT_BOARD_SIZE);
+        columnSlider.setBounds(284, 105, 158, 26);
+        columnSlider.setFocusable(false);
+        columnSlider.setMajorTickSpacing(10);
+        columnSlider.setMinorTickSpacing(2);
+        columnSlider.setPaintTicks(true);
+        columnSlider.addChangeListener(this);
+        contentPane.add(columnSlider);
+        
+        rowNumLabel = new JLabel(DEFAULT_BOARD_SIZE + "");
+        rowNumLabel.setFont(RESULT_FONT);
+        rowNumLabel.setBounds(445, 83, 22, 16);
+        contentPane.add(rowNumLabel);
+        
+        columnNumLabel = new JLabel(DEFAULT_BOARD_SIZE + "");
+        columnNumLabel.setFont(RESULT_FONT);
+        columnNumLabel.setBounds(445, 110, 22, 16);
+        contentPane.add(columnNumLabel);
+        
+        browseButton = new JButton("Browse...");
+        browseButton.setBounds(220, 78, 97, 25);
+        browseButton.addActionListener(this);
+        browseButton.setVisible(false);
+        contentPane.add(browseButton);
+        
+        fileLabel = new JLabel("default.txt");
+        fileLabel.setFont(RESULT_FONT);
+        fileLabel.setBounds(328, 78, 114, 25);
+        fileLabel.setVisible(false);
+        contentPane.add(fileLabel);
+        
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       if (e.getSource() == randomBoardButton || e.getSource() == fileBoardButton) {
+           rowsLabel.setVisible(e.getSource() == randomBoardButton);
+           rowSlider.setVisible(e.getSource() == randomBoardButton);
+           rowNumLabel.setVisible(e.getSource() == randomBoardButton);
+           columnsLabel.setVisible(e.getSource() == randomBoardButton);
+           columnSlider.setVisible(e.getSource() == randomBoardButton);
+           columnNumLabel.setVisible(e.getSource() == randomBoardButton);
+           
+           browseButton.setVisible(e.getSource() == fileBoardButton);
+           fileLabel.setVisible(e.getSource() == fileBoardButton);
+       }
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        if (e.getSource() == rowSlider)
+            rowNumLabel.setText(rowSlider.getValue() + "");
+        else if (e.getSource() == columnSlider)
+            columnNumLabel.setText(columnSlider.getValue() + "");
     }
     
     public static void main(String[] args) throws UnknownHostException {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e){
+            // Use default look and feel.
+        }
+        
         new MinesweeperServerFrame();
     }
 }
